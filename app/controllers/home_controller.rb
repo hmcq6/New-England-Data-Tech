54cba3aa801ef7b2bfcbd353aa47bc98fe9e2245
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-04-15 23:50:48 -0400

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+    respond_to :html
+  end
+
+  def services
+    respond_to :html
+  end
+
+  def products
+    respond_to :html
+  end
+
+  def gallery
+    respond_to :html
+  end
+
+  def contact
+    respond_to :html
+  end
+
+end

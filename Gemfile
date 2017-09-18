d92c6e5e9aec6aec3fad55b1cd859d4201daf519
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-09-17 20:18:19 -0400

    Add basic photo gallery

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 
+gem 'sqlite3'
+
+gem 'carrierwave'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'services', to: 'home#services'
-  get 'gallery', to: 'home#gallery'
+  get 'gallery', to: 'photos#index'
   get 'contact', to: 'home#contact'
 end

ba9b19a2e5e1f47ad596db2a4a0896dbacb65998
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-10-01 19:18:18 -0400

    Remove Users Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  resources :users
   resources :sessions
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

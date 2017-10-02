0d3508659f9273ccab6628e56f3ac221ec63f434
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-10-01 21:42:09 -0400

    Add user creation route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   get 'login', to: 'sessions#new'
+  resources :users
   resources :sessions
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
